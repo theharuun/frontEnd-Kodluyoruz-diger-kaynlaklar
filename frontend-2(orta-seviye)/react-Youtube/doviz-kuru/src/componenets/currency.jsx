e5bd6dbfacee7662defc57e03b6302bdd86e0352
theharuun <160717559+theharuun@users.noreply.github.com>
2024-05-09 19:42:58 +0300

    Doviz-Kuru uygulamasi bitti

--- frontend-2(orta-seviye)/react-Youtube/doviz-kuru/src/componenets/currency.jsx
+++ frontend-2(orta-seviye)/react-Youtube/doviz-kuru/src/componenets/currency.jsx
@@ -1,30 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/currency.css'
 import { FaLongArrowAltRight } from "react-icons/fa";
+import axios from 'axios';
+
+
+
+let BASE_URL="https://api.freecurrencyapi.com/v1/latest";
+let API_KEY="fca_live_akdPQa0eRhiqWo0FX3AiKqAvQgGY7i1PHAqpez6F"
+
 
 function Currency() {
+   
+    const [amount,setAmount]=useState();
+    const [fromCurrency,setFromCurrency]=useState('USD');
+    const [toCurrency,setToCurrency]=useState('TRY');
+    const [result,setResult]=useState(0);
+    
+// let EXCHANGE_API=cd 
+
+    const exChange=async()=>{
+        // console.log(amount);
+        // console.log(fromCurrency);
+        // console.log(toCurrency);
+
+        const response=await axios.get(`${BASE_URL}?apikey=${API_KEY}&base_currency=${fromCurrency}`);
+        const res= (response.data.data[toCurrency]*amount).toFixed(2);  // .dan sonra 2 basamak olucak şekilde yuvarlama yapılıyor
+        setResult(res);
+      
+      
+        // const res=async()=>{ await response.data.data[toCurrency] * amount}
+        // axios.put(result,async()=>{ await response.data.data[toCurrency] * amount}); // setResult var
+
+    }
+
+
+
   return (
     <div className='currency-div'>
         <div>
             <h3>DÖVİZ KURU UYGULAMASI</h3>
         </div>
         <div style={{paddingInline: '19px' ,}} >
-        <input type="number" className='amount' />
-        <select className='from-currency-option'>
+        <input 
+        value={amount}
+        onChange={(e)=>{setAmount(e.target.value)}}
+        type="number" className='amount' />
+        <select 
+        onChange={(e)=>{setFromCurrency(e.target.value)}}
+        className='from-currency-option'>
         <option>USD</option>
-        <option>TL</option>
+        <option>TRY</option>
         <option>EUR</option>
 
         </select>
-        <button className='btn-donustur'>  <FaLongArrowAltRight /></button>
 
-        <select className='to-currency-option'>
-        <option>TL</option> 
+
+        <button 
+        onClick={exChange}
+        className='btn-donustur'>  <FaLongArrowAltRight /></button>
+
+        <select 
+        onChange={(e)=>{setToCurrency(e.target.value)}}
+        className='to-currency-option'>
+        <option>TRY</option> 
         <option>USD</option> 
         <option>EUR</option>
 
         </select>
-        <input type="number" className='result' />
+        <input
+        value={result}
+        onChange={(e)=>{setResult(e.target.value)}}
+        type="number" className='result' />
         </div>
         </div>
   )
